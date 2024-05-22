f424d75bfa24ff5f2f1d8a4badf960c283735560
mor <mori1236@gmail.com>
2024-05-22 22:54:05 +0300

    fixed errors

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,38 +1,32 @@
 import { Link } from "react-router-dom"
 import Navbar from "../components/Navbar"
 import '../styles/HomePage.css'
-import Title from "../assets/mainTitle.svg"
 
 function HomePage() {
-    const homeText = `מחזירים את האור למפונים מהדרום והצפון
-
-    אנחנו עוטף ישראל.
-    
-    אנו מלווים משפחות מהדרום ומהצפון ונותנים להם מעטפת של 360 מעלות בתחומים הבאים: בריאות הגוף והנפש, תרבות, חינוך, ביגוד, ציוד ועוד. בסיוע של גופים מובילים במשק, אנו מספקים פעילויות חינוכיות וטכנולוגיות, 3 ארוחות ביום ופעילויות חברתיות כגון: ספורט, יצירה, דרמה, הופעות, ריקוד ועוד... 
-    
-    אם אתם מעוניינים לתאם פעילות, מתנדבים להעביר פעילות כלשהיא, או זקוקים לכל סיוע אחר מאיתנו, אתם מוזמנים לפנות אלינו בטפסים כאן למטה - יש ללחוץ על הכפתור המתאים ולמלא את הטופס
-    (חמ"ל עוטף יכול לסייע לכם גם בדברים נוספים, מוזמנים לדבר איתנו גם לגבי זה)
-    
-    סייענו לכ-2500 תושבי הדרום והצפון ב-18 בתי מלון בתל אביב, ובנינו לוחות זמנים להפעלת ההורים והילדים בכל יום.`
-
-    const minText = ``;
     return (
         <>
             <Navbar/>
             <div id="main-grid">
-                {/* <h4>{homeText}</h4> */}
-                <img height={'400px'} style={{justifySelf:"center"}} src="../src/assets/mainTitle.svg"/>
-                <div>
-                    <video width="200" controls>
-                        <source src="../src/assets/hamal-video.mp4" type="video/mp4"/>
-                    </video>
+                <img style={{justifySelf:"center", height:"inherit"}} src="../src/assets/mainTitle.svg"/>
+                <div style={{justifyContent:"center", justifySelf:"center",height:"300px", position:"relative"}}>
+                    <div className="brown" style={{transform:"translate(28px,-28px)"}} ></div>
+                    <div className="brown" style={{transform:"translate(-28px,28px)" , left:"0px",bottom:"0px"}}></div>
+                    <div style={{justifyContent:"center",borderRadius:"20px",overflow:"hidden", justifySelf:"center", height:"inherit"}}>
+                        <video style={{height:"inherit",position:"relative"}} controls>
+                            <source src="../src/assets/hamal-video.mp4" type="video/mp4"/>
+                        </video>
+                    </div>
                 </div>
-                {/* <Title/> */}
-                {/* <p id="main-text">{minText}</p> */}
-
+                <div>
+                    <div className="text" style={{fontSize:"32px", paddingBottom:"20px"}}>אנחנו חמ"ל עוטף ישראל.</div>
+                    <div className="text" style={{fontSize:"24px", fontWeight:"300", marginRight:"40px"}}>סייענו לכ-2500 תושבי הדרום והצפון ב-18 בתי מלון בתל אביב, ובנינו לוחות זמנים להפעלת ההורים והילדים בכל יום.</div>
+                </div>
             </div>
-            <Link id="route-link" to={"/form/offer"}>לסיוע</Link>
-            <Link id="route-link" to={"/form/request"}>לבקשת עזרה</Link>
+            <div className="buttons">
+                <Link id="route-link-offer" className="button" to={"/form/offer"}>לסיוע</Link>
+                <Link id="route-link-request" className="button" to={"/form/request"}>לבקשת עזרה</Link>
+            </div>
+
  
         </>
     )
