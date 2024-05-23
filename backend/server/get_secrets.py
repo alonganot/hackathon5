29c0e377e47b82f869aa73da83594dba3460bb44
Adi Eliyahu <213668692@idf.il>
2024-05-23 05:01:07 +0300

    testing config

--- backend/server/get_secrets.py
+++ backend/server/get_secrets.py
@@ -1,17 +1,26 @@
-from kubernetes import client, config
+from json import load
+from pathlib import Path
+from kubernetes import client
+
 
 class GetSecrets:
     def __init__(self, secret_name) -> None:
-        # Load Kubernetes/OpenShift configuration from the default location
-        config.load_kube_config()
+        try:
+            # Load Kubernetes/OpenShift configuration from the default location
+            config.load_kube_config()
 
-        # Create an instance of the Kubernetes client
-        core_api = client.CoreV1Api()
+            # Create an instance of the Kubernetes client
+            core_api = client.CoreV1Api()
 
-        namespace = 'sayeret'
+            namespace = 'sayeret'
 
-        # Retrieve a specific secret by name
-        secret = core_api.read_namespaced_secret(secret_name, namespace)
+            # Retrieve a specific secret by name
+            secret = core_api.read_namespaced_secret(secret_name, namespace)
 
-        # Decode and access the secret data
-        self.decoded_data = {key: value.decode('utf-8') for key, value in secret.data.items()}
+            # Decode and access the secret data
+            self.decoded_data = {key: value.decode('utf-8') for key, value in secret.data.items()}
+
+        except Exception:
+            current_dir: Path = Path(__file__).parent
+            with open(current_dir.joinpath('config.json')) as config_file:
+                config = load(config_file)
